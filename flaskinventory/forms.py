85b6062847605ed6560ca5d2f3a7ce33fdfc13c9
astalkb <202280176@psu.palawan.edu.ph>
2025-05-31 20:16:50 +0800

    Added addproduct

--- flaskinventory/forms.py
+++ flaskinventory/forms.py
@@ -7,6 +7,7 @@ from wtforms.validators import DataRequired,NumberRange
 class addproduct(FlaskForm):
     prodname = StringField('Product Name', validators=[DataRequired()])
     prodqty = IntegerField('Quantity', validators=[NumberRange(min=5, max=1000000),DataRequired()])
+    prodlocation = SelectField('Initial Location')
     prodsubmit = SubmitField('Save Changes')
 
 class editproduct(FlaskForm):
