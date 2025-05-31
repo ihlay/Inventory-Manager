d32f780db1b8b77dc21b71bea545a5a0d4e41b96
astalkb <202280176@psu.palawan.edu.ph>
2025-05-31 16:30:06 +0800

    Fix the database table

--- flaskinventory/__init__.py
+++ flaskinventory/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '323b22caac41acbf'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -11,5 +8,6 @@ db = SQLAlchemy(app)
 
 from flaskinventory import routes
 
-db.create_all()
-db.session.commit()
+with app.app_context():
+    db.create_all()
+    db.session.commit()
