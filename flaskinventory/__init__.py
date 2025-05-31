47c3b8c97b1702a87dcf82d1601e20db13e2edf5
astalkb <202280176@psu.palawan.edu.ph>
2025-05-31 16:19:48 +0800

    Fix the URI

--- flaskinventory/__init__.py
+++ flaskinventory/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '323b22caac41acbf'
-app.config['SQLALCHEMY'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 from flaskinventory import routes
